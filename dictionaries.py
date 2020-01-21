e255e0afe58fb3101234c0c151dcb5cab25e62c1
MGalati <46815903+MGalati@users.noreply.github.com>
2020-01-21 15:36:49 +0100

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # http://rosalind.info/problems/ini6/
 
+
 def count_words(text):
     dict = {}
     for words in text:
@@ -9,9 +10,9 @@ def count_words(text):
     for words, counts in dict.items():
         print(words, counts)
 
+        
 if __name__ == '__main__':
     f = open('G:\\Téléchargement\\rosalind_ini6.txt', 'r')
     raw_input = f.read()
     text = raw_input.split()
-
     print(count_words(text))
