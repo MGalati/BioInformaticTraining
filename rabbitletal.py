ee1e056a82589ca6899f1bbb99d02b6f6b8c69e6
MGalati <46815903+MGalati@users.noreply.github.com>
2020-01-21 17:21:45 +0100

    Create rabbitletal.py

---
+++ rabbitletal.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# http://rosalind.info/problems/fibd/
+
+
+def mortal_fib(months, lifetime):
+    ages = [0] * lifetime
+    ages[-1] = 1
+    for i in range(1, months):
+        new_rabbits = sum(ages[:-1])
+        ages[:-1] = ages[1:]  # Shift ages left is for rabbits getting older
+        ages[-1] = new_rabbits
+    return sum(ages)
+
+
+if __name__ == '__main__':
+    print(mortal_fib(96, 16))
