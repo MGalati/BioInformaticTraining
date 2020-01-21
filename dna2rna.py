916e174ccf703e9c66aa5ede40ddc7352f9c77dd
MGalati <46815903+MGalati@users.noreply.github.com>
2020-01-21 15:23:40 +0100

    Create dna2rna.py

---
+++ dna2rna.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# http://rosalind.info/problems/rna/
+
+def dna2rna(dna):
+    rna = dna.replace("T", "U")
+    return rna
+
+if __name__ == '__main__':
+    f = open('G:\\Téléchargement\\test.txt', 'r')
+    dna = f.read()
+    print(dna2rna(dna))
