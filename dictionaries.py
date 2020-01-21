f994196dd007aa6d01d75936c89c5aea90f872cf
MGalati <46815903+MGalati@users.noreply.github.com>
2020-01-21 14:52:12 +0100

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# http://rosalind.info/problems/ini6/
+
+def count_words(text):
+    dict = {}
+    for words in text:
+        dict[words] = text.count(words)
+
+    for words, counts in dict.items():
+        print(words, counts)
+
+if __name__ == '__main__':
+    f = open('G:\\Téléchargement\\rosalind_ini6.txt', 'r')
+    raw_input = f.read()
+    text = raw_input.split()
+
+    print(count_words(text))
