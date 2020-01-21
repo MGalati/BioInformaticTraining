3f4cc3be6aab9cdbd31f6778d0f608124918c3ce
MGalati <46815903+MGalati@users.noreply.github.com>
2020-01-21 15:35:34 +0100

    Update compl_dna.py

--- compl_dna.py
+++ compl_dna.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python
 # http://rosalind.info/problems/revc/
 
-def compl_dna(dna):
-    dna_lenght=len(dna)
-    slicedDNA=dna[dna_lenght::-1] # Backward the str
-    compldna=''
 
-    for i in slicedDNA:
-        if i=="A":
+def compl_dna(dna):
+    dna_lenght = len(dna)
+    sliceddna = dna[dna_lenght::-1] # Backward the str
+    compldna = ''
+
+    for i in sliceddna:
+        if i == "A":
             compldna += "T"
-        elif i=="T":
+        elif i == "T":
             compldna += "A"
-        elif i=="G":
+        elif i == "G":
             compldna += "C"
         elif i == "C":
             compldna += "G"
 
     return compldna
+
 
 if __name__ == '__main__':
     f = open('G:\\Téléchargement\\test.txt', 'r')
